89e655319470622d421ca4fb6451e61a1b85baeb
Achiugo Trust <achiugotrust@gmail.com>
2025-06-11 11:45:34 +0100

    some changes

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 from google import genai
 
 load_dotenv()
-api_key = os.environ.get("AIzaSyD58d2GUrzR8AVIoWoMaMDhaaVnMeJaKFk")
-client = genai.Client(api_key="AIzaSyD58d2GUrzR8AVIoWoMaMDhaaVnMeJaKFk")
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
 
 
 if len(sys.argv) < 2:
@@ -20,4 +20,3 @@ response = client.models.generate_content(
 print(response.text)
 print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
 print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
