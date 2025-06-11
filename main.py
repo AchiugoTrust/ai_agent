909ea71b7dfd57ec071c08dc28c3c3fcceefdba6
Achiugo Trust <achiugotrust@gmail.com>
2025-06-11 11:02:21 +0100

    enabled to ask questions from cli

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,8 +8,14 @@ api_key = os.environ.get("AIzaSyD58d2GUrzR8AVIoWoMaMDhaaVnMeJaKFk")
 client = genai.Client(api_key="AIzaSyD58d2GUrzR8AVIoWoMaMDhaaVnMeJaKFk")
 
 
+if len(sys.argv) < 2:
+    print("Usage: python script.py 'Ask your question here'")
+    sys.exit(1)
+
+question = sys.argv[1]
+
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    model='gemini-2.0-flash-001', contents=question
 )
 print(response.text)
 print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
