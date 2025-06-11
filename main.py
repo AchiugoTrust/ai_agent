e713bb8c07b0afa21e7b2f63dc543fba7b60f587
Achiugo Trust <achiugotrust@gmail.com>
2025-06-11 11:52:37 +0100

    some changes

--- main.py
+++ main.py
@@ -18,5 +18,5 @@ response = client.models.generate_content(
     model='gemini-2.0-flash-001', contents=question
 )
 print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+"""print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")"""
 print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
