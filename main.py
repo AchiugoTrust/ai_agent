fecefcb4032b91640bb8d242bb41660c5a54f08d
Achiugo Trust <achiugotrust@gmail.com>
2025-06-11 16:05:08 +0100

    calculator

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 
+
 load_dotenv()
+
+
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
+    
 
 if len(sys.argv) < 2:
     print("Usage: python script.py 'Ask your question here'")
@@ -17,6 +21,14 @@ question = sys.argv[1]
 response = client.models.generate_content(
     model='gemini-2.0-flash-001', contents=question
 )
+
+if "--verbose" in sys.argv:
+    print(f"User prompt: {question}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+"""
 print(response.text)
-"""print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")"""
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
 print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+"""
